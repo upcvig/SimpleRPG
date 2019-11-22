1134f7d0d36be244a29c54fb7293d608e9c85469
upcvig <alex.rios.jerez@gmail.com>
2019-11-22 18:16:30 +0100

    Version 1

---
+++ SimpleRPG/Assets/Scripts/MovementBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+
+        _rigidbody.AddForce(new Vector2(movX, movY), ForceMode2D.Force);
+        _animator.SetFloat("speed", System.Math.Abs(movX + movY));
+    }
+}
